a142be4a1db03475e169037c16c79b771dc33bf7
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2022-07-15 22:30:46 +0300

    Add files via upload

---
+++ mathmatical-operation.dart
@@ -0,0 +1,14 @@
+void main() {
+  int quntity = 3;
+  double price = 10.52;
+
+  print(quntity + price); // add
+
+  print(quntity - price); //sub
+
+  print(quntity * price); //mul
+
+  print(quntity / price); //div
+
+  print(quntity % price); //mod
+}
